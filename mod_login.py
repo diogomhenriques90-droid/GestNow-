92000b1e0aafcbf0c451b85a1eb21a4a038daa49
diogomhenriques90-droid <diogomhenriques90@gmail.com>
2026-04-01 10:27:58 +0100

    Update mod_login.py

--- mod_login.py
+++ mod_login.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import secrets
 from core import load_all, cp, hp, t, ICONS, COLORS
-
+from translations import t
 def render_login():
     """Renderiza página de login com design industrial moderno"""
     
